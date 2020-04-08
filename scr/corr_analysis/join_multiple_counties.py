80298e2171f7356f262e268fec407df2c44549d1
Luyu Liu <liuluyu0378@gmail.com>
2020-04-08 11:25:47 -0400

    Upload DMA topojson

--- scr/corr_analysis/join_multiple_counties.py
+++ scr/corr_analysis/join_multiple_counties.py
@@ -13,6 +13,20 @@ col_system = db_corona.system_info
 col_case = db_corona.case_count
 col_ridership = db_corona.ridership
 
+col_occu = db_corona.census_transit_pop
+
 rl_system = col_system.find({})
 for each_system in rl_system:
     system_name =  each_system["name"]
+    try:
+        county_FIPS = each_system["county_FIPS"]
+    except:
+        continue
+
+    rl_occu = col_occu.find_one({"county_FIPS": int(county_FIPS)})
+    try:
+        wfh_pop = rl_occu["transit_pop"]
+    except:
+        print(county_FIPS)
+    all_pop = rl_occu["all_pop"]
+    print('"', system_name ,'"', wfh_pop, all_pop)
